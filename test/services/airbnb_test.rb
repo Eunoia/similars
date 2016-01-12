241f042301f11c20a235d38f260c3c533e6bf758
Evan R <eunoia.github@gmail.com>
2016-01-12 12:18:46 -0800

    style edits

--- test/services/airbnb_test.rb
+++ test/services/airbnb_test.rb
@@ -4,19 +4,19 @@ class TestAirbnb < Minitest::Test
   end
 
   def test_that_listing_returns_important_attributes
-    VCR.use_cassette("listing") do
-      listing = Airbnb.listing("4693285")
-      keys_we_need = %w{city country name smart_location address zipcode id}
+    VCR.use_cassette('listing') do
+      listing = Airbnb.listing('4693285')
+      keys_we_need = %w(city country name smart_location address zipcode id)
       values = listing.values_at(*keys_we_need).compact
 
-      assert_equal values.length, 7, "There is a mismatch of keys"
+      assert_equal values.length, 7, 'There is a mismatch of keys'
     end
   end
 
   def test_that_similar_listings_has_many_listings
-  	VCR.use_cassette("similar") do
-  		similar_ids = Airbnb.similar_listings("4693285")
-  		assert_equal similar_ids.length, 24, "There is no keys"
-  	end
+    VCR.use_cassette('similar') do
+      similar_ids = Airbnb.similar_listings('4693285')
+      assert_equal similar_ids.length, 24, 'There is no keys'
+    end
   end
 end
